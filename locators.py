271344989ae4c5e01c96d466b642d3935c529c38
CarlosAPenaM <carlospenauvm@gmail.com>
2025-05-23 17:58:24 -0600

    test final

---
+++ locators.py
@@ -0,0 +1,32 @@
+from selenium.webdriver.common.by import By
+
+class Locators:
+
+    from_field = (By.ID, 'from')
+    to_field = (By.ID, 'to')
+    request_taxi_button = (By.CSS_SELECTOR, ".button.round")
+    comfort_rate_icon = (By.XPATH, "//div[@class='tcard-title' and text()='Comfort']")
+    phone_number_field = (By.CLASS_NAME, "np_text")
+    phone_number_field_popup = (By.ID, "phone")
+    phone_number_field_button = (By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/div[1]/form/div[2]/button')
+
+    Siguiente_button = (By.XPATH, "//button[text()='Siguiente']")
+    codigo_sms_field = (By.XPATH, "//*[@id='code']")
+    Confirmar_button = (By.XPATH, "//button[text()='Confirmar']")
+
+    payment_method_button = (By.CSS_SELECTOR, ".pp-button")
+    agregar_tarjeta_button = (By.CSS_SELECTOR, ".pp-plus-container")
+    card_number_field = (By.ID, "number")
+    cvv_field = (By.NAME, "code")
+    agregar_button = (By.XPATH, "//div[@class='pp-buttons']/button[text()='Agregar']")
+    close_button_popup = (By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[1]/button")
+
+    message_for_driver_field = (By.ID, "comment")
+    requisitos_button = (By.CLASS_NAME, "reqs-head")
+    manta_y_panuelos_slider = (
+    By.XPATH, "//*[@id='root']/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[1]/div/div[2]/div/span")
+    plus_icecream = (By.XPATH, "//*[@id='root']/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div/div[3]")
+    pedir_un_taxi_last_button = (By.CLASS_NAME, "smart-button")
+
+
+
---
+++ test.py
@@ -0,0 +1,72 @@
+from selenium.webdriver.common import keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+import data
+from selenium import webdriver
+from selenium.webdriver import Keys
+from locators import Locators
+from UrbanRoute import UrbanRoutesPage
+
+
+
+class TestUrbanRoutes:
+
+    driver = None
+
+    @classmethod
+    def setup_class(cls):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("perfLoggingPrefs", {'enableNetwork': True, 'enablePage': True})
+        chrome_options.set_capability("goog:loggingPrefs", {'performance': 'ALL'})
+        cls.driver = webdriver.Chrome(options=chrome_options)
+        cls.driver.get(data.urban_routes_url)
+        cls.routes_page = UrbanRoutesPage(cls.driver)
+
+    def test_set_route(self):
+        address_from = data.address_from
+        address_to = data.address_to
+        self.routes_page.set_route(address_from, address_to)
+        assert self.routes_page.get_from() == address_from
+        assert self.routes_page.get_to() == address_to
+
+    def test_click_pedir_un_taxi(self):
+        self.routes_page.set_phone_number(data.phone_number)
+
+    def test_select_confort_rate(self):
+        self.routes_page.get_comfort_rate()
+
+        comfort_text = "comfort"
+        comfort_icon = self.routes_page.get_comfort_rate().text
+        assert comfort_text in comfort_icon
+
+    def test_set_phone_number(self):
+        self.routes_page.set_phone_number(data.phone_number)
+
+    def test_set_payment_method(self):
+        self.routes_page.open_payment_method_popup()
+        self.routes_page.click_agregar_tarjeta()
+        self.routes_page.set_card_number(data.card_number)
+        assert self.routes_page.set_card_number(data.card_number).get_property('value') == data.card_number
+        self.routes_page.set_cvv_number(data.card_code)
+        self.routes_page.get_cvv_field().send_keys(keys.TAB)
+        self.routes_page.click_agregar_button()
+        self.routes_page.close_popup_payment_method()
+
+    def test_set_mensaje_al_conductor(self):
+        self.routes_page.set_mensaje_al_conductor(data.message_for_driver)
+        assert self.routes_page.get_mensaje_al_conductor() == data.message_for_driver
+
+    def test_order_manta_y_panuelos(self):
+        self.routes_page.set_manta_y_panuelos()
+        #assert routes_page.get_manta_y_panuelos().get_property('checked')
+
+    def test_order_dos_helados(self):
+        self.routes_page.set_icecream()
+
+    def test_click_pedir_taxi_final(self):
+        self.routes_page.click_pedir_un_taxi_final()
+
+    @classmethod
+    def teardown_class(cls):
+        cls.driver.quit()
